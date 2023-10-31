d65ff682b595f254d130517f27ad41ed075b4278
kwishna <kwishna@gmail.com>
2023-10-31 22:05:55 +0530

    almost finished

--- src/main/java/rest/cucumber/handles/Responder.java
+++ src/main/java/rest/cucumber/handles/Responder.java
@@ -1,12 +1,18 @@
 package rest.cucumber.handles;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.ResponseSpecification;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class Responder {
 
@@ -22,6 +28,72 @@ public class Responder {
     public Responder validateResponse(Response response, ResponseSpecification respSpec) {
         response.then().spec(respSpec);
         return this;
+    }
+
+    /**
+     * Get JsonPath from Response.
+     *
+     * @param response
+     * @return
+     */
+    public JsonPath getBodyAsJsonPath(Response response) {
+        return response.body().jsonPath();
+    }
+
+    public <T> T getBody(Response response, Class<T> clazz) {
+        return response.body().as(clazz);
+    }
+
+    public <T> T getBody(Response response, Type cls) {
+        return response.body().as(cls);
+    }
+
+    public String getContentType(Response response) {
+        return response.contentType();
+    }
+
+    public Headers getHeaders(Response response) {
+        return response.headers();
+    }
+
+    public <T> T extractFromBody(Response response, String path, Class<T> objectType) {
+        return response.body().jsonPath().getObject(path, objectType);
+    }
+
+    public String extractStringFromBody(Response response, String path) {
+        return response.body().jsonPath().getString(path);
+    }
+
+    public <T> List<T> extractListFromBody(Response response, String path, Class<T> clazz) {
+        return response.body().jsonPath().getList(path, clazz);
+    }
+
+    public <T> T extractFromBody(Response response, String path) {
+        return response.body().path(path);
+    }
+
+    public Map<Object, Object> extractMapFromBody(Response response, String path) {
+        return response.body().jsonPath().getMap(path);
+    }
+
+    public int getStatusCode(Response response) {
+        return response.statusCode();
+    }
+
+    public String getStatusList(Response response) {
+        return response.statusLine();
+    }
+
+    public long getResponseTime(Response response) {
+        return response.time();
+    }
+
+    public long getResponseTime(Response response, TimeUnit timeUnit) {
+        return response.getTimeIn(timeUnit);
+    }
+
+    public String getResponseAsString(Response response) {
+        return response.getBody().asString();
     }
 
     /**
