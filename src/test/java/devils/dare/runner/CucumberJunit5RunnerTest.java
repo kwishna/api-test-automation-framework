955f58ab7057de8e7e830f4e76627e426e20dbac
kwishna <kwishna@gmail.com>
2024-06-15 15:48:15 +0530

    Junit5

--- src/test/java/devils/dare/runner/CucumberJunit5RunnerTest.java
+++ src/test/java/devils/dare/runner/CucumberJunit5RunnerTest.java
@@ -1,27 +1,28 @@
 package devils.dare.runner;
 
-//import org.junit.platform.suite.api.*;
-// 
-//import static io.cucumber.junit.platform.engine.Constants.*;
-//import io.cucumber.core.plugin.SerenityReporterParallel;
-//@Suite(failIfNoTests = true)
-//@SuiteDisplayName("Cucumber JUnit5")
-//@IncludeEngines("cucumber")
-// // @SelectDirectories("src/test/java/features")
-// // @SelectClasspathResource("io/github/bonigarcia")
-//@ConfigurationParameters({
-//        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "devils.dare.hooks"),
-//        @ConfigurationParameter(key = OBJECT_FACTORY_PROPERTY_NAME, value = "io.cucumber.picocontainer.PicoFactory"),
-//        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "devils.dare.stepDefs"),
-//        @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features")
-//})
-// // @ExcludeClassNamePatterns({"*Disabled*"})
-// // @ExcludePackages({"devils.dare.disabled"})
-// // @ExcludeTags({"skip"})
-// // @IncludeTags("all")
-// // @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@ui")
-//public class CucumberJunit5RunnerTest {
-//}
+import org.junit.platform.suite.api.*;
+import static io.cucumber.junit.platform.engine.Constants.*;
+import io.cucumber.core.plugin.SerenityReporterParallel;
+
+@Suite(failIfNoTests = true)
+@SuiteDisplayName("Cucumber JUnit5")
+@IncludeEngines("cucumber")
+ // @SelectDirectories("src/test/java/features")
+ // @SelectClasspathResource("io/github/bonigarcia")
+@ConfigurationParameters({
+        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "devils.dare.hooks"),
+        @ConfigurationParameter(key = OBJECT_FACTORY_PROPERTY_NAME, value = "io.cucumber.picocontainer.PicoFactory"),
+        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "devils.dare.stepDefs"),
+        @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features"),
+        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:build/cucumber-results/timeline,html:build/cucumber-results/cucumber-report.html,com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:")
+})
+ // @ExcludeClassNamePatterns({"*Disabled*"})
+ // @ExcludePackages({"devils.dare.disabled"})
+ // @ExcludeTags({"skip"})
+ // @IncludeTags("all")
+ // @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@ui")
+public class CucumberJunit5RunnerTest {
+}
 /*
 Alternate Ways To Pass Cucumber Parameters:-
 1) mvn -Dtest="devils.dare.runner.CucumberJunit5Test" test
